da30c214af8e9371b1dac86f711d1a522887b105
Ujjal Kumar Dutta <duttaujjal143@gmail.com>
2023-04-04 17:09:38 +0530

    Added necessary files again

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 ## Prompt user to change to zsh
-[ "${SHELL##/*/}" == "zsh" ] && echo 'Change default shell to zsh using: `chsh -s /bin/zsh`'
+[ "${SHELL##/*/}" != "zsh" ] && echo 'Change default shell to zsh using: `chsh -s /bin/zsh`'
 
 ## Below lines of code pull the dotfile repo and install various configurations in your computer
-dir="$HOME/peronal"
+dir="$HOME/personal"
 mkdir -p $dir # Create teh subdirectories if not present
 
-git clone --recursive https://
+git clone --recursive https://github.com/UD-UD/dotfilesv2.git
+
+cd dotfilesv2
+
+sh etc/symlink_dotfiles.sh
