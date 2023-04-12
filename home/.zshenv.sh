70c3baa122e52d698c427c393987032cfce80d72
Ujjal Dutta <duttaujjal143@gmail.com>
2023-04-12 16:25:50 +0530

    Removing compdef temporality

--- home/.zshenv.sh
+++ home/.zshenv.sh
@@ -1,3 +1,4 @@
 
 
 # The .zshenv is used every time you start zsh. This is for your environment variables like $PATH, $EDITOR, $VISUAL, $PAGER, $LANG.
+. "$HOME/.cargo/env"
--- terminal/git-alias.sh
+++ terminal/git-alias.sh
@@ -1,51 +1,51 @@
 # Aliases
 alias g='git'
-compdef g=git
+# compdef g=git
 alias gst='git status'
-compdef _git gst=git-status
+# compdef _git gst=git-status
 alias gl='git pull'
-compdef _git gl=git-pull
+# compdef _git gl=git-pull
 alias gup='git fetch && git rebase'
-compdef _git gup=git-fetch
+# compdef _git gup=git-fetch
 alias gp='git push'
-compdef _git gp=git-push
+# compdef _git gp=git-push
 gdv() { git diff -w "$@" | view - }
-compdef _git gdv=git-diff
+# compdef _git gdv=git-diff
 alias gc='git commit -v'
-compdef _git gc=git-commit
+# compdef _git gc=git-commit
 alias gca='git commit -v -a'
-compdef _git gca=git-commit
+# compdef _git gca=git-commit
 alias gco='git checkout'
-compdef _git gco=git-checkout
+# compdef _git gco=git-checkout
 alias gcm='git checkout master'
 alias gb='git branch'
-compdef _git gb=git-branch
+# compdef _git gb=git-branch
 alias gba='git branch -a'
-compdef _git gba=git-branch
+# compdef _git gba=git-branch
 alias gcount='git shortlog -sn'
-compdef gcount=git
+# compdef gcount=git
 alias gcp='git cherry-pick'
-compdef _git gcp=git-cherry-pick
+# compdef _git gcp=git-cherry-pick
 alias glg='git log --stat --max-count=5'
-compdef _git glg=git-log
+# compdef _git glg=git-log
 alias glgg='git log --graph --max-count=5'
-compdef _git glgg=git-log
+# compdef _git glgg=git-log
 alias gss='git status -s'
-compdef _git gss=git-status
+# compdef _git gss=git-status
 alias ga='git add'
-compdef _git ga=git-add
+# compdef _git ga=git-add
 
 alias gaa='git add --all'
-compdef _git gaa='git-add --all'
+# compdef _git gaa='git-add --all'
 
 alias gm='git merge'
-compdef _git gm=git-merge
+# compdef _git gm=git-merge
 alias grh='git reset HEAD'
 alias grhh='git reset HEAD --hard'
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
-compdef git-svn-dcommit-push=git
+# compdef git-svn-dcommit-push=git
 
 alias gsr='git svn rebase'
 alias gsd='git svn dcommit'
@@ -66,24 +66,24 @@ function current_repository() {
 
 # these aliases take advantage of the previous function
 alias ggpull='git pull origin $(current_branch)'
-compdef ggpull=git
+# compdef ggpull=git
 alias ggpush='git push origin $(current_branch)'
-compdef ggpush=git
+# compdef ggpush=git
 alias ggpnp='git pull origin $(current_branch) && git push origin $(current_branch)'
-compdef ggpnp=git
+# compdef ggpnp=git
 
 
 alias gr='git rm'
-compdef _git gr=git-status
+# compdef _git gr=git-status
 
 alias gf='git fetch'
-compdef _git gf=git-status
+# compdef _git gf=git-status
 
 alias gss='git status --short'
-compdef _git gss=git-status
+# compdef _git gss=git-status
 
 alias gd='git diff'
-compdef _git gd=git-status
+# compdef _git gd=git-status
 
 alias gdisc='git discard'
-compdef _git gdisc=git-status
+# compdef _git gdisc=git-status
